d93dffdc9792973f832bfad7cef21e0ba767ca64
MohdAimann <97003700+MohdAimann@users.noreply.github.com>
2024-11-05 09:54:16 +0000

    Update

---
+++ Booleans.java
@@ -0,0 +1,9 @@
+public class Booleans {
+	public static void main(String[] args) { 
+    //variable true or false    
+    boolean intsCanHoldDecimals = false;
+
+    //print the data
+    System.out.println(intsCanHoldDecimals);
+	}
+}
---
+++ Song.java
@@ -0,0 +1,9 @@
+public class Song {
+	public static void main(String[] args) 
+  {   
+    //Create Variable
+    String openingLyrics = "Yesterday, all my troubles seemed so far away";
+    //Print the data
+    System.out.println(openingLyrics);
+	}
+}
