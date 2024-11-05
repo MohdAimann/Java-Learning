5dad248927e5b1bc361ccd80049cc5662eef9a0f
MohdAimann <97003700+MohdAimann@users.noreply.github.com>
2024-11-05 09:21:52 +0000

    Update

---
+++ Tree.java
@@ -0,0 +1,18 @@
+// Define your tree class in this file!
+public class Tree{
+    public static void main(String[] args)
+    {
+      /* We're going to introduce ourselves and print a tree to the screen */
+      System.out.println("My name is Aiman");
+      System.out.println("I'm going to plant a tree today!");
+      //Draw the tree code goes here
+      System.out.println(" *****");
+      System.out.println(" ***** ");
+      System.out.println(" ***** ");
+      System.out.println(" ***** ");
+      System.out.println("   *  ");
+      System.out.println("   *  ");
+      System.out.println("#######"); 
+  
+    }
+  }
